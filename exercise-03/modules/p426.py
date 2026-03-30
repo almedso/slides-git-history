63f00f04551ad533908a04321b013a24271e6191
Volker Kempert <volker.kempert@almedso.de>
2026-03-30 21:56:08 +0200

    Add p400-p449

---
+++ exercise-03/modules/p426.py
@@ -0,0 +1,8 @@
+A = 300
+B = 42
+
+def add():
+    return A + B
+
+def subtract():
+    return A - B
---
+++ exercise-03/modules/p428.py
@@ -0,0 +1,8 @@
+A = 302
+B = 42
+
+def add():
+    return A + B
+
+def subtract():
+    return A - B
