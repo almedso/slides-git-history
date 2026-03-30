05ad3cd1416de33369e1e21ae3e68fc2cc30b928
Volker Kempert <volker.kempert@almedso.de>
2026-03-30 21:53:03 +0200

    Add p700-p749

---
+++ exercise-03/modules/p708.py
@@ -0,0 +1,8 @@
+A = 300
+B = 136
+
+def add():
+    return A + B
+
+def subtract():
+    return A - B
---
+++ exercise-03/modules/p712.py
@@ -0,0 +1,8 @@
+A = 301
+B = 137
+
+def add():
+    return A + B
+
+def subtract():
+    return A - B
